b8596cb38cfaaa0ad509b10861cf2a0f55c6c768
hhshams <hhshams1333@gmail.com>
2024-07-05 18:01:13 +0330

    changing tsconfig, add setupKafkaConsumer method

--- src/workers/workers.ts
+++ src/workers/workers.ts
@@ -1,15 +1,39 @@
-import {Kafka, logLevel} from "kafkajs";
+import {Consumer, EachMessageHandler, Kafka, logLevel} from "kafkajs";
 import config from "config";
+import {logger} from "../../utils/globalLogger";
 
+// Connect to kafka brokers 
 export const kafka = new Kafka({
     logLevel: logLevel.INFO,
     clientId: 'pigeon',
     brokers: config.get("KAFKA_BROKERS"),
 });
 
+// Main Worker Class who actually does the main work
 abstract class Worker{
+    consumer: Consumer | undefined = undefined;
 
-    protected constructor(name:string,groupId:string,topics:string[] ) {
+    protected constructor(protected name:string,protected groupId:string,protected topics:string[],protected messageHandler:EachMessageHandler ) {
+        this.setupKafkaConsumer()
       
+    }
+
+    // Create A Kafak Consumer and subscribes to topics
+    async setupKafkaConsumer(){
+        try{
+            this.consumer = kafka.consumer({ groupId: this.groupId });
+            
+            await this.consumer.connect()
+            logger.info( this.name+ " Consumer connected ")
+
+            await this.consumer.subscribe({topics: this.topics})
+            logger.info(this.name+" Consumer subscribed to "+ this.topics);
+
+            await this.consumer.run({eachMessage: this.messageHandler})
+            logger.info(this.name+" Consumer started processing messages !!")
+
+        }catch (e) {
+            logger.error(e)
+        }
     }
 }
